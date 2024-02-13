28887cffe18a9422f5a8678ab5ac9a0b09a2ce4a
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-02-13 17:26:07 -0300

    fix frozen string literal linter

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class MessagesController < ApplicationController
   before_action :set_message, only: %i[show edit update destroy]
 
--- app/views/messages/index.json.jbuilder
+++ app/views/messages/index.json.jbuilder
@@ -1,3 +1,1 @@
-# frozen_string_literal: true
-
 json.array! @messages, partial: 'messages/message', as: :message
